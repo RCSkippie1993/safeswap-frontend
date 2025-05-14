0865478482b2c5762447e0d0f1bfdcbb5dc46509
Rikus Scheepers <rikusscheepers@Rikuss-MacBook-Air.local>
2025-05-14 09:08:14 +0200

    Initial frontend commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Welcome to SafeSwap</h1>
+      <p>Your secure platform to buy and sell goods.</p>
+    </div>
+  );
+}
+
+export default App;
